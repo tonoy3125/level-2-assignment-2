0d15f1990d9123a39e9eb065236970c96566b551
tonoy3125 <shaifshajedt@gmail.com>
2024-06-24 22:50:28 +0600

    product get service added

--- src/app/product/product.service.ts
+++ src/app/product/product.service.ts
@@ -1,11 +1,23 @@
 import { TProduct } from './product.interface'
 import { Product } from './product.model'
 
+// Create Product Service
 const createProduct = async (payload: TProduct) => {
   const result = await Product.create(payload)
   return result
 }
 
+// Get Product Service
+const getAllProducts = async (searchTerm: unknown) => {
+  if (typeof searchTerm === 'string') {
+    const result = Product.find({ $text: { $search: searchTerm } })
+    return result
+  }
+  const result = Product.find()
+  return result
+}
+
 export const productServices = {
   createProduct,
+  getAllProducts,
 }
