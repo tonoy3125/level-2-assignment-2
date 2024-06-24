5f7519c3c62dcbd2b7a381c55ae5c8405a536a18
tonoy3125 <shaifshajedt@gmail.com>
2024-06-24 19:53:34 +0600

    variants validationschema validation added

--- src/app/product/product.validation.ts
+++ src/app/product/product.validation.ts
@@ -2,12 +2,18 @@ import { z } from 'zod'
 
 
 
+const variantsValidationSchema = z.object({
+  type: z.string().min(1, 'type is required'),
+  value: z.string().min(1, 'value is required'),
+})
+
 export const productValidationSchema = z.object({
   name: z.string().min(1, 'Name is required'),
   description: z.string().min(1, 'Description is required'),
   price: z.number().min(1, 'Prize is required'),
   category: z.string().min(1, 'Category is required'),
   tags: z.string().array(),
+  variants: z.array(variantsValidationSchema).min(1, 'varitants are required'),
   
 })
 
