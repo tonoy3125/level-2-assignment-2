be1b986e9eadece0877cb428de8ccc4426490a61
tonoy3125 <shaifshajedt@gmail.com>
2024-06-25 13:50:50 +0600

    create order controller added also route added commit

---
+++ src/app/order/order.controller.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from 'express'
+import { orderValidationSchema } from './order.validation'
+import { orderServices } from './order.service'
+
+const createOrder = async (req: Request, res: Response) => {
+  try {
+    const data = req?.body
+    const order = orderValidationSchema.parse(data)
+    const result = await orderServices.createOrder(order)
+    res.json({
+      success: true,
+      message: 'Order created successfully!',
+      data: result,
+    })
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (err: any) {
+    if (err.name === 'ZodError') {
+      return res.status(500).json({
+        success: false,
+        message: err,
+      })
+    }
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    })
+  }
+}
+
+export const orderControllers = {
+  createOrder,
+}
