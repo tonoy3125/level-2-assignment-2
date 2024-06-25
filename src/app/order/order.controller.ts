51aa3dbf1c1d8745d335c4e2f125a1dfc813dc9b
tonoy3125 <shaifshajedt@gmail.com>
2024-06-25 18:48:42 +0600

    product find by id middleware added also inventory management logic added

--- src/app/order/order.controller.ts
+++ src/app/order/order.controller.ts
@@ -27,7 +27,47 @@ const createOrder = async (req: Request, res: Response) => {
   }
 }
 
+const getAllOrders = async (req: Request, res: Response) => {
+  try {
+    const { email } = req.query
+    const result = await orderServices.getAllOrders(email)
+    if (email) {
+      if (!result) {
+        return res.status(500).json({
+          success: false,
+          message: 'Order not found',
+        })
+      } else {
+        if (result.length === 0) {
+          return res.json({
+            success: false,
+            message: 'no order found for this user email!',
+            data: result,
+          })
+        }
+        return res.json({
+          success: true,
+          message: 'Orders fetched successfully for user email!',
+          data: result,
+        })
+      }
+    }
+
+    res.json({
+      success: true,
+      message: 'Orders fetched successfully!',
+      data: result,
+    })
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: 'something went wrong',
+      data: err,
+    })
+  }
+}
 
 export const orderControllers = {
   createOrder,
+  getAllOrders,
 }
--- src/app/order/order.model.ts
+++ src/app/order/order.model.ts
@@ -1,5 +1,6 @@
 import { Schema, model } from 'mongoose'
 import { TOrder } from './order.interface'
+import { Product } from '../product/product.model'
 
 const orderSchema = new Schema<TOrder>({
   email: {
@@ -18,6 +19,44 @@ const orderSchema = new Schema<TOrder>({
     type: Number,
     required: [true, 'quantity is required'],
   },
+})
+
+orderSchema.pre('save', async function (next) {
+  const result = await Product.findById(this.productId)
+  if (!result) {
+    throw new Error('Product does not exists by this productId')
+  }
+  // checks if the requested quantity is greater then the product quantity
+
+  const {
+    inventory: { quantity },
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  }: any = await Product.findById(this.productId)
+
+  if (quantity < this.quantity) {
+    throw new Error('Insufficient quantity available in inventory')
+  }
+
+  // reduce the product quantity
+  const updatedProduct = await Product.findByIdAndUpdate(
+    this.productId,
+    {
+      $inc: {
+        'inventory.quantity': -this.quantity,
+      },
+    },
+    { new: true },
+  )
+  // update the instock if quantity is 0
+  if (updatedProduct?.inventory.quantity === 0) {
+    await Product.findByIdAndUpdate(this.productId, {
+      $set: {
+        'inventory.inStock': false,
+      },
+    })
+  }
+
+  next()
 })
 
 export const Order = model<TOrder>('Order', orderSchema)
